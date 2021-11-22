51167a155b8e22cdd566ea2bcbb1cfa902c541ae
yeyun <yeynii@gmail.com>
2021-11-23 04:40:04 +0900

    11.23/ 레이아웃 수정

--- src/components/books_manager/book_lists/book_lists.jsx
+++ src/components/books_manager/book_lists/book_lists.jsx
@@ -4,20 +4,22 @@ import styles from "./book_lists.module.css";
 
 const BookLists = ({ books, removeBook }) => {
   return (
-    <ul className={styles.books}>
-      <div className={styles.fields}>
-        <div className={styles.title}>제목</div>
-        <div className={styles.price}>교재비</div>
-      </div>
-      <div className={styles.bookLists}>
-      {books &&
-        Object.keys(books)
-        .sort((a, b) => books[a].title > books[b].title? 1: -1)
-        .map(key => (
-          <BookList key={key} book={books[key]} removeBook={removeBook}/>
-        ))}
-      </div>
-    </ul>
+    <div className={styles.container}>
+      <div className={styles.columnName}>
+      <div className={styles.title}>제목</div>
+      <div className={styles.price}>교재비</div></div>
+      <ul className={styles.books}>
+        <div className={styles.fields}></div>
+        <div className={styles.bookLists}>
+          {books &&
+            Object.keys(books)
+              .sort((a, b) => (books[a].title > books[b].title ? 1 : -1))
+              .map((key) => (
+                <BookList key={key} book={books[key]} removeBook={removeBook} />
+              ))}
+        </div>
+      </ul>
+    </div>
   );
 };
 
