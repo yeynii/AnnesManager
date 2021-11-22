c2df4f5734513fa4379add2bc643d86f9745d867
yeyun <yeynii@gmail.com>
2021-11-23 04:18:31 +0900

    11.23/ 학생목록 drawer 기능 추가

--- src/components/students/students.jsx
+++ src/components/students/students.jsx
@@ -3,50 +3,82 @@ import styles from "./students.module.css";
 import StudentAddForm from "./student_add_form/student_add_form";
 import Student from "./student/student";
 import SearchBar from "../search_bar/SearchBar";
+import { HiMenu } from "react-icons/hi";
 
-const Students = ({ createOrUpdateStudent, students, openInformation, selectedId }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [keyword, setKeyword] =useState('');
+const Students = ({
+  createOrUpdateStudent,
+  students,
+  openInformation,
+  selectedId,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  const [isDrawerOn, setIsDrawerOn] = useState(false);
+
+  const handleDrawer = () => {
+    setIsDrawerOn(!isDrawerOn);
+  };
 
   const closeModal = () => {
-    setModalIsOpen(false);
-  }
+    setIsModalOpen(false);
+  };
 
-  const getSearchedStudents = word => {
+  const getSearchedStudents = (word) => {
     setKeyword(word);
-  }
+  };
 
   return (
-    <section className={styles.students}>
-      <h1 className={styles.title}>학생 목록</h1>
-      <SearchBar getSearchedStudents={getSearchedStudents}/>
-      <ul className={styles.studentList}>
-        {students && Object.keys(students)
-        .filter(key => students[key].name.includes(keyword))
-        .sort((a, b) => students[a].name > students[b].name? 1: -1)
-        .map((key) => (
-          <Student
-            key={key}
-            student={students[key]}
-            openInformation={openInformation}
-            selectedId={selectedId}
-          />
-        ))}
-      </ul>
-      <button
-        className={styles.addStudents}
-        onClick={() => setModalIsOpen(true)}
-      >
-        +
+    <>
+      <button className={`${styles.drawer} ${getDrawerstyle(isDrawerOn)}`} onClick={handleDrawer} >
+        <HiMenu />
       </button>
-      <StudentAddForm
-        modalIsOpen={modalIsOpen}
-        closeModal={closeModal}
-        createOrUpdateStudent={createOrUpdateStudent}
-      />
-      
-    </section>
+      <section className={`${styles.students} ${getStudentsstyle(isDrawerOn)}`}>
+        <h1 className={styles.title}>학생 목록</h1>
+        <SearchBar getSearchedStudents={getSearchedStudents} />
+        <ul className={styles.studentList}>
+          {students &&
+            Object.keys(students)
+              .filter((key) => students[key].name.includes(keyword))
+              .sort((a, b) => (students[a].name > students[b].name ? 1 : -1))
+              .map((key) => (
+                <Student
+                  key={key}
+                  student={students[key]}
+                  openInformation={openInformation}
+                  selectedId={selectedId}
+                />
+              ))}
+        </ul>
+        <button
+          className={styles.addStudents}
+          onClick={() => setIsModalOpen(true)}
+        >
+          +
+        </button>
+        <StudentAddForm
+          isModalOpen={isModalOpen}
+          closeModal={closeModal}
+          createOrUpdateStudent={createOrUpdateStudent}
+        />
+      </section>
+    </>
   );
 };
+
+function getDrawerstyle(isDrawerOn) {
+  if (isDrawerOn == true){
+    return styles.onDrawer;
+  } 
+  else
+    return styles.offDrawer;
+
+} 
+function getStudentsstyle(isDrawerOn) {
+  if (isDrawerOn == true){
+    return styles.studentsOn;
+  } 
+  else
+    return styles.studentsOff;
+} 
 
 export default Students;
